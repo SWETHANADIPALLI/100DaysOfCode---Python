d554ddd5eff934e761407f8145f30b1d4d119267
Swetha Nadipalli <80444974+SWETHANADIPALLI@users.noreply.github.com>
2022-08-17 01:29:10 +0530

    Add files via upload

---
+++ Day 1/Day 1 - Solutions/Name Generator.py
@@ -0,0 +1,5 @@
+#Greeting message
+print('Hello, Welcome to Full Name Genearator')
+first_name=input('Enter the First Name: ')
+last_name=input('Enter the Last Name:')
+print('The Generated Full Name is: '+first_name+" "+last_name)
